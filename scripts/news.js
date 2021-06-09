2fefd833a737017f8e84e1ba3cf89582dd497057
VampirePenguinSociety <vampirepenguinsociety666@gmail.com>
2021-06-09 16:29:44 -0500

    Actually finished about page (forgot to fill out some info earlier)

--- scripts/news.js
+++ scripts/news.js
@@ -1,7 +1,7 @@
 const announcements = [
     {
         title: "New Website!",
-        content: ['With the end of the academic year coming to a close for some on the admin team, they are faced with final projects for various classes. One of these admins happens to be taking a web development course. and so, this site was born.',
+        content: ['With the end of the academic year coming to a close for some on the admin team, they are faced with final projects for various classes. One of these admins happens to be taking a web development course, and so, this site was born.',
         'It\'s still very much a work in progress and not everything is implemented yet, but if you see something that should work not working, please let the guild admin team know.']
     }
 ]
