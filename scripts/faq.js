519ba9393a830d7320a51e2d42eac62a9b658631
VampirePenguinSociety <vampirepenguinsociety666@gmail.com>
2021-06-09 13:28:15 -0500

    fixed some styles, populated Q&A page

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,16 +1,72 @@
-let QAFilePath = ""
-let QASection = ``
+Vue.component('questions-list', {
+    props: ['type', 'question_list'],
+    template:
+    `
+    <article>
+        <h2>{{type}}</h2>
+        <question-answer-pair v-for="qa in question_list" :key="qa.number" :number="qa.number" :question="qa.question" :answer="qa.answer" :type="type"></question-answer-pair>
+    </article>
+    `
+})
 
-function getFormattedQA(question, answer) {
+Vue.component('question-answer-pair', {
+    props: ['number', 'question', 'answer', 'type'],
+    template:
+    `
+    <div>
+    <p class="question" :id="question_id">{{number}}. {{question}}</p>
+    <p class="answer" v-for="a in answer">{{a}}</p>
+    </div>
+    `,
+    computed: {
+        question_id() {
+            return this.type + "_" + this.number.toString()
+        }
+    }
+})
 
-}
+Vue.component("quick-nav-section", {
+    props: ['type', 'question_list'],
+    template: `
+    <div>
+        <h3>{{type}}</h3>
+        <ol>
+            <quick-nav-question-link v-for="qa in question_list" :key="qa.number" :number="qa.number" :type="type" :question="qa.question"></quick-nav-question-link>
+        </ol>
+    </div>
+    `
+})
 
-function getFormattedArticle(title, QAList) {
+Vue.component("quick-nav-question-link", {
+    props: ['type', 'number', 'question'],
+    template: `
+    <li><a :href="question_id">{{question}}</a></li>
+    `,
+    computed: {
+        question_id() {
+            return "#" + this.type + "_" + this.number.toString()
+        }
+    }
+})
 
-}
-
-function initFAQ() {
-    
-}
-
-//initFAQ()
+new Vue({
+    el: '#main',
+    template: `
+        <main id="main">
+            <section>
+                <h1>Frequently Asked Questions</h1>
+                <questions-list v-for="qalist in QAList" :key="qalist.type" :type="qalist.type" :question_list="qalist.QA"></questions-list>
+            </section>
+            <aside>
+                <article id="quick_nav">
+                    <h2>Q&A Quick Nav</h2>
+                    <quick-nav-section v-for="qalist in QAList" :key="qalist.type" :type="qalist.type" :question_list="qalist.QA"> </quick-nav-section>
+                    </div>
+                </article>
+            </aside>
+        </main>
+    `,
+    data: {
+        QAList: QAList
+    }
+})
---
+++ scripts/faq_questionlist.js
@@ -0,0 +1,120 @@
+const QAList = [
+    {
+        type: 'General',
+        QA: [
+            {
+                number: 1,
+                question: 'Are there any requirements or dues for members?',
+                answer: ['There are no requirements or dues for members. Just have fun!']
+            },
+            {
+                number: 2,
+                question: 'What is the play style of VPS?',
+                answer: ["Casual. We know life happens, and we don't want to add stress to that for anyone."]
+            },
+            {
+                number: 3,
+                question: 'Does VPS have a guild hall?',
+                answer: ["We have two: The one by our Guildmaster @Space Keet, which is her Primary residence at Stillwaters Retreat. It has all crafting stations (yes, even jewellery!) but no set stations, Mundus stones or Transmute station. If you are looking for the latter three options, our Affiliated Guildmaster at Klub Kerfuffle @PaxTheWise has allowed us to promote his Guildhall for such matters.", "(Guildmaster Space Keet does not have the time, patience, crowns, gold, OR irl money to get all of the set stations.)"]
+            }
+        ]
+    },
+    {
+        type: 'Discord',
+        QA: [
+            {
+                number: 1,
+                question: "Why can't I post in the #Trials or #Mini-Events channel?",
+                answer: ["In order to keep signups for trials and rosters at the bottom of the channel so that everyone can see them easily, the channel is locked unless you're a raidmaster.  Use #events-discussion to post questions about or discuss upcoming trials."]
+            },
+            {
+                number: 2,
+                question: 'How can I get discord admin rank?',
+                answer: ["Not with that attitude, that's for sure."]
+            }
+        ]
+    },
+    {
+        type: 'Bank',
+        QA: [
+            {
+                number: 1,
+                question: 'How does VPS use its guild bank?',
+                answer: ["The VPS guild bank is a place where members are welcome to share items that they don't need for other members of the guild to freely take—please remember to be considerate!  All members should be able to both deposit and withdraw items—no need to ask an officer."]
+            }
+        ]
+    },
+    {
+        type: 'Trials',
+        QA: [
+            {
+                number: 1,
+                question: 'What kind of trials does VPS host?',
+                answer: ['We host normals trials as well as Open Veteran trials for you to sign up in, just check the #Trials channel in the discord for sign-ups. All information regarding the difficulty, date, time and trial will be there.']
+            },
+            {
+                number: 2,
+                question: 'Are there requirements for signing up for trials?',
+                answer: ["In general, members who join trials are expected to have properly levelled and specced gear and be at least lvl 50 CP 160 unless stated otherwise. For normal trials, it's okay if your gear isn't all the correct trait, etc. However, you're always expected to follow the rules for events. Also, if you're appearing anonymously in the encounter logs, the Raidmasters will be very sad. Please don't make us sad."]
+            },
+            {
+                number: 3,
+                question: "Oh no! I can't make it to a trial I signed up for! What should I do!?",
+                answer: ["First off, relax. We know life happens. We prefer having at least a 24-hour warning if you can't make it to the trial, but if something urgent pops up, we understand. Just let us know in the #events-discussion channel on Discord and ping the trial host.",
+                "That said... if you repeatedly cancel last-minute or are found to be cancelling without good cause, we will be sad, as it means we may need to cancel events last-minute due to not having enough people. If you do this too many times, your signup attempts will be de-prioritized or ignored to make space for people who actually plan to attend."]
+            },
+            {
+                number: 4,
+                question: 'How many trials does VPS host each week?',
+                answer: ['We try to host 2-4 trials a week, depending on the availability of hosts and participants.']
+            },
+            {
+                number: 5,
+                question: 'How can I get notified whenever trials stuff happens?',
+                answer: ['If you wish to be notified when trials are happening, the #Events-info channel has a reactive role application to cane the Vampguin Raider role. Admin cannot give the role because otherwise, the bot throws a hissyfit.']
+            },
+            {
+                number: 6,
+                question: "Can I request a particular trial?",
+                answer: ["Anyone is now allowed to request any trial they'd like to be scheduled in the #events-discussion channel. When putting in a request, please list the time frame, the days you would like the trial to be held, and whether or not you would like it to be a farm run. As far as veteran trials go, if the progression group has not already cleared it and no Vampguin Raidmaster feels comfortable leading it, we will let you know! (Keep in mind that not all requests can be done immediately.)"]
+            }
+        ]
+    },
+    {
+        type: 'Progression Group',
+        QA: [
+            {
+                number: 1,
+                question: 'Does VPS have a progression group?',
+                answer: ["We do, actually! It's the VPS Mudballers. We run progression trials weekly(ish) on Sundays 5-7 PM CDT. Guild Admin Higgsbo is in charge of organizing these runs."]
+            },
+            {
+                number: 2,
+                question: 'How can I join the progression team?',
+                answer: ['Currently, the VPS Mudballers are not recruiting. Furthermore, much of our recruitment is done on an invite basis, with Raidmasters picking new members based on previous trials they have ran with the guild.',
+                "If you have any questions on the process or about the team, please direct them to Guild Admin Higgsbo."]
+            },
+            {
+                number: 3,
+                question: "Does not being in the progression guild mean I can't run harder trials?",
+                answer: ["As the Mudballers progression team advances and clears more content, tougher trials that can be comfortably cleared by the team will start to be added to the signups that are open to the rest of the guild."]
+            },
+        ]
+    },
+    {
+        type: 'Events',
+        QA: [
+            {
+                number: 1,
+                question: 'Does VPS host events other than trials?',
+                answer: ["Much of our focus right now is on trials. However, time-permitting, we also hope to host other PvE events, such as world boss runs, dolmen runs, etc."]
+            },
+            {
+                number: 2,
+                question: 'Does VPS host PvP events?',
+                answer: ['Generally, no. PvP is not something the guild leaders want to focus on.']
+            }
+        ]
+    }, 
+
+]
